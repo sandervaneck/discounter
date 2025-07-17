cdc3767ea857ebc861d0a92769522077eb46661b
sandervaneck <69575096+sandervaneck@users.noreply.github.com>
2025-07-17 11:19:24 +0200

    fix

--- src/app/components/Toolbar.tsx
+++ src/app/components/Toolbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-export const RestaurantToolbar = ({tab, setTab}: {tab: number, setTab: (n: number) => void}) => {
-
-    return (
+export const RestaurantToolbar = ({ tab, setTab }: { tab: number; setTab: (n: number) => void }) => {
+  return (
     <div className="flex justify-between items-center px-8 py-4 mb-8 bg-white rounded-2xl shadow-md border border-gray-100">
       {/* Left section */}
       <div className="text-lg font-semibold text-gray-800">
@@ -13,13 +12,21 @@ export const RestaurantToolbar = ({tab, setTab}: {tab: number, setTab: (n: numbe
       <div className="flex gap-4">
         <a
           href="/restaurant"
-          className={`bg-red-500 hover:bg-red-600 ${tab === 0 ? 'text-white bg-[#ff715b]' : 'bg-white text-[#ff715b] border border-[#ff715b] hover:bg-[#fff2f0]'} px-4 py-2 rounded-lg text-sm font-medium transition`}
+          className={`bg-red-500 hover:bg-red-600 ${
+            tab === 0
+              ? 'text-white bg-[#ff715b]'
+              : 'bg-white text-[#ff715b] border border-[#ff715b] hover:bg-[#fff2f0]'
+          } px-4 py-2 rounded-lg text-sm font-medium transition`}
         >
           Manage Discount Codes
         </a>
         <a
           href="/cashier"
-          className={`bg-red-500 hover:bg-red-600 ${tab === 1 ? 'text-white bg-[#ff715b]' : 'bg-white text-[#ff715b] border border-[#ff715b] hover:bg-[#fff2f0]'} px-4 py-2 rounded-lg text-sm font-medium transition`}
+          className={`bg-red-500 hover:bg-red-600 ${
+            tab === 1
+              ? 'text-white bg-[#ff715b]'
+              : 'bg-white text-[#ff715b] border border-[#ff715b] hover:bg-[#fff2f0]'
+          } px-4 py-2 rounded-lg text-sm font-medium transition`}
         >
           Validate Discount Code
         </a>
